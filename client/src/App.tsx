84cdf48d7ad97c4144544f4f2239481368d7311e
Shashwat <shashwatw.1906@gmail.com>
2025-05-10 17:58:33 +0530

    removing node modules from github

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Login from "./auth/Login"
 import Signup from "./auth/Signup"
-import MainLayout from "./MainLayout"
 import ForgotPassword from "./auth/ForgotPassword"
 import ResetPassword from "./auth/ResetPassword"
 import VerifyEmail from "./auth/VerifyEmail"
