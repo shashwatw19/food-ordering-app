f49efc0d948015844cb79c25dbdd8703e6e694ae
Shashwat Wawge <shashwatw.1906@gmail.com>
2025-08-08 12:18:42 +0530

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,46 +1,46 @@
-import express, { Request, Response, NextFunction } from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
-dotenv.config();
+import express, { Request, Response, NextFunction } from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const app = express();
-app.use(cors({
+const app = express()
+app.use(
+  cors({
     origin: process.env.ORIGIN || 'http://localhost:5173',
-    credentials : true
-}));
+    credentials: true,
+  })
+)
 
-app.use(express.json());
+app.use(express.json())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-
-app.get('/api/v1' , (req: Request, res: Response) => {
-    res.send('Food Ordering API is running!')
+app.get('/api/v1', (req: Request, res: Response) => {
+  res.send('Food Ordering API is running!')
 })
 
-import userRouter from '../src/routes/user.routes'
-import restaurantRouter from '../src/routes/restaurant.routes'
-import menuRouter from '../src/routes/menu.routes'
-import paymentRouter from '../src/routes/payments.routes'
-import orderRouter from '../src/routes/order.routes'
+import userRouter from './routes/user.routes.js'
+import restaurantRouter from './routes/restaurant.routes.js'
+import menuRouter from './routes/menu.routes.js'
+import paymentRouter from './routes/payments.routes.js'
+import orderRouter from './routes/order.routes.js'
 
-app.use('/api/v1/user' , userRouter)
-app.use('/api/v1/restaurant' , restaurantRouter)
-app.use('/api/v1/menu' , menuRouter)
+app.use('/api/v1/user', userRouter)
+app.use('/api/v1/restaurant', restaurantRouter)
+app.use('/api/v1/menu', menuRouter)
 app.use('/api/v1/payment', paymentRouter)
-app.use('/api/v1/order' , orderRouter)
-
+app.use('/api/v1/order', orderRouter)
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    const statusCode = err.statusCode || 500;
-    
-    res.status(statusCode).json({
-        success: false,
-        message: err.message || "Internal Server Error",
-        errors: err.errors || [],
-        statusCode : statusCode
-    });
-});
-export {app}
+  const statusCode = err.statusCode || 500
+
+  res.status(statusCode).json({
+    success: false,
+    message: err.message || 'Internal Server Error',
+    errors: err.errors || [],
+    statusCode: statusCode,
+  })
+})
+export { app }
