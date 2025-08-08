f2d01eeb5a5d55c429b044e6a16baaa23313da13
Shashwat Wawge <shashwatw.1906@gmail.com>
2025-08-08 12:29:01 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,16 @@
-import {app} from './app'
-import { dbConnect } from '../src/db/dbConnect'
+import { app } from './app'
+import { dbConnect } from './db/dbConnect'
 import dotenv from 'dotenv'
-import { User } from './models/user.model'
 dotenv.config()
 
-const PORT = process.env.PORT || 3000 
+const PORT = process.env.PORT || 3000
 
-dbConnect().
-then(()=>{
-    app.listen(PORT, ()=>{
-        console.log(`Server is running on http://localhost:${PORT}`)
+dbConnect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`)
     })
-})
-.catch((err: unknown)=>{
-    console.log(`Error white connecting to database!` , err)
-})
-
-
-
+  })
+  .catch((err: unknown) => {
+    console.log(`Error white connecting to database!`, err)
+  })
